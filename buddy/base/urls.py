6679a9a41ed1cec0f269d77fed4d4cd187cce3a8
PiyushVedwal7 <piyushvedwal11@gmail.com>
2024-12-27 02:06:03 +0530

    updates till night

--- buddy/base/urls.py
+++ buddy/base/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
+    path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message'),
 
 
     
